b05a4be20280a386b506be121d5545d710d4c3d5
coderSasidharan <66076670+coderSasidharan@users.noreply.github.com>
2020-12-26 11:03:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+import ScanScreen from "./screens/ScanScreen";
+
+
+export default class App extends React.Component {
+  render() {
+    return <ScanScreen/>;
+  }
+}
+
